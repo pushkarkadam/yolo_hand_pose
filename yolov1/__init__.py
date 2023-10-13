b1f697958c8737df2c02774878a434b5765ddc02
Pushkar Kadam <pushkarkadam17@outlook.com>
2023-10-13 17:25:10 +1100

    changed to relative import

--- yolov1/__init__.py
+++ yolov1/__init__.py
@@ -1,14 +1,5 @@
-import yolov1.model
-from yolov1.model import *
-
-import yolov1.loss
-from yolov1.loss import *
-
-import yolov1.utils
-from yolov1.utils import * 
-
-import yolov.train 
-from yolov1.train import *
-
-import yolov1.dataset 
-from yolov1.dataset import *
+from .model import *
+from .loss import *
+from .utils import *
+from .train import *
+from .dataset import *
