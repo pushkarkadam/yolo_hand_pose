ca6f30fa9b704d12a8e86208bebdde9146da9911
Pushkar Kadam <pushkarkadam17@outlook.com>
2023-11-01 19:21:43 +1100

    time elapsed added

--- yolov1/train.py
+++ yolov1/train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torchvision.transforms.functional as FT 
 from tqdm import tqdm 
 from torch.utils.data import DataLoader
+import time
 from .model import *
 from .dataset import *
 from .utils import *
@@ -29,6 +30,7 @@ LABEL_DIR = "../data/labels"
 def train(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
     model.train()
+    dt = time.time()
     for batch, (X, y) in enumerate(dataloader):
         X = X.to(device)
         y = y.type(torch.LongTensor)
@@ -44,8 +46,9 @@ def train(dataloader, model, loss_fn, optimizer, device):
         optimizer.zero_grad()
         
         if batch % 100 == 0:
+            time_elapsed = time.time() - dt
             loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}] [Time Elapsed: {time_elapsed/3600:>4f} h]")
 
 def test(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
@@ -54,13 +57,15 @@ def test(dataloader, model, loss_fn, device):
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
+            X = X.to(device)
+            y = y.type(torch.LongTensor) 
+            y = y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
+    print(f"Test Error: \nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
 
 class Compose(object):
     def __init__(self, transforms):
