ac5dfafb73022f4b47773fd936772f8c166375bb
Pushkar Kadam <pushkarkadam17@outlook.com>
2024-02-02 17:05:38 +0000

    yolo loss for box and class added

--- yolov1/loss.py
+++ yolov1/loss.py
@@ -4,7 +4,8 @@ from .utils import intersection_over_union
 
 
 class YoloLoss(nn.Module):
-    def __init__(self, S=7, B=2, C=20):
+    """YOLO loss"""
+    def __init__(self, S=7, B=2, C=2, K=21):
         super(YoloLoss, self).__init__()
         self.mse = nn.MSELoss(reduction="sum")
         self.S = S
@@ -12,91 +13,79 @@ class YoloLoss(nn.Module):
         self.C = C
         self.lambda_noobj = 0.5
         self.lambda_coord = 5
+        self.lamda_keypoint = 0.5
         
     def forward(self, predictions, target):
-        predictions = predictions.reshape(-1, self.S, self.C + self.B * 5)
-        # IoU for bounding box 1
-        # Because we select two bounding boxes
-        # 0 - 19: 20 classes of the object detection problem
-        # 20: Class score
-        # 21 - 25: Four bounding box values [x y w h] for bounding box 1
-        # 26 - 30: Four bounding box values [x y w h] for bounding box 2
-        # TODO: Not an efficient solution. Cannot scale up the number of bounding boxes.
-        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
-        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
-        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
-        # IoU max values, argmax of the IoU
-        iou_maxes, best_box = torch.max(ious, dim=0)
-        exists_box = target[..., 20].unsqueeze(3) # identitiy of object i
+        pred = yolo_head(predictions, num_boxes=B, num_landmarks=K, num_classes=C, grid_size=S)
         
-        # ---------------
-        # Box coordinates
-        # ---------------
-        box_predictions = exists_box * (
-            (
-                best_box * predictions[..., 26:30]
-                + (1 - best_box) * predictions[..., 21:25]
-            )
-        )
+        # Extracting Prediction
+        pred_boxes_xy = pred['bboxes_xy']
+        pred_boxes_wh = pred['bboxes_wh']
+        pred_boxes_classes = pred['classes']
+        pred_boxes_conf = pred['confidence']
         
-        box_targets = exists_box * target[..., 21:25]
+        # Extracting Ground Truth Target
+        target_box_xy = target['box_xy']
+        target_box_wh = target['box_wh']
+        target_box_classes = target['classes_gt']
+        target_box_confidence = target['confidence_gt']
         
-        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(torch.abs(box_predictions[..., 2:4] + 1e-6))
+        target_box_corners = yolo_boxes_to_corners(target_box_xy, target_box_wh)
         
+        boxes_iou = []
         
-        # (N, S, S, 25) => (..., :25) --> gets all the N, S, S dimensions
-        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+        for pred_box_xy, pred_box_wh in zip(pred_boxes_xy, pred_boxes_wh):
+            pred_box_corners = yolo_boxes_to_corners(pred_box_xy, pred_box_wh)
+            
+            box_iou = iou(pred_box_corners, target_box_corners)
+            
+            boxes_iou.append(box_iou)
+            
+        boxes_iou = [b.unsqueeze(0) for b in boxes_iou]
+    
+        boxes_iou = torch.cat(boxes_iou, dim=0)
+
+        iou_maxes, best_box_index = torch.max(boxes_iou, dim=0)
         
-        # (N, S, S, 4) -> (N*S*S, 4)
-        box_loss = self.mse(
-            torch.flatten(box_predictions, end_dim=-2),
-            torch.flatten(box_targets, end_dim=-2),
-        )
+        # Indicator function that is used to test if the object exists
+        exists_box = target['confidence_gt']
         
-        # -----------
-        # Object loss
-        # -----------
+        # Squeezing the box dimensions to convert from (1, 2, S, S) to (2, S, S)
+        pred_boxes_xy = [box.squeeze(0) for box in pred_boxes_xy]
+        pred_boxes_wh = [box.squeeze(0) for box in pred_boxes_wh]
         
-        pred_box = (
-            best_box * predictions[..., 25:26] + (1 - best_box) * predictions[..., 20:21]
-        )
-        
-        # (N*S*S)
-        object_loss = self.mse(
-            torch.flatten(exists_box * pred_box),
-            torch.flatten(exists_box * target[..., 20:21])
-        )
-        
-        # --------------
-        # No object loss
-        # --------------
-        
-        # (N, S, S, 1) -> (N, S*S)
-        no_object_loss = self.mse(
-            torch.flatten((1-exists_box) * prediction[..., 20:21], start_dim=1),
-            torch.flatten((1-exists_box) * target[..., 20:21], start_dim=1)
-        )
-        
-        no_object_loss += self.mse(
-            torch.flatten((1-exists_box) * prediction[..., 25:26], start_dim=1),
-            torch.flatten((1-exists_box) * target[..., 20:21], start_dim=1)
-        )
-        
-        # ----------
-        # Class loss
-        # ----------
-        
-        # (N, S, S, 20) -> (N*S*S, 20)
-        class_loss = self.mse(
-            torch.flatten(exists_box * predictions[..., :20], end_dim=-2),
-            torch.flatten(exists_box * target[..., :20], end_dim=-2)
-        )
-        
-        loss = (
-            self.lambda_coord * box_loss
-            + object_loss
-            + self.lambda_noobj * no_object_loss
-            + class_loss
-        )
+        # Finding the box responsible for prediction
+        predictor_xy = predictor_box(pred_boxes_xy, best_box_index.squeeze(0))
+        predictor_wh = predictor_box(pred_boxes_wh, best_box_index.squeeze(0))
+        predictor_conf = predictor_box(pred_boxes_conf, best_box_index.squeeze(0))
+        # TODO: Implement predictor_box function for keypoints
+        # predictor_k = predictor_box(t)
+
+        # Coordinate losses
+        # xy loss
+        predictor_xy = exists_box * predictor_xy
+        xy_loss = self.lambda_coord * self.mse(torch.flatten(predictor_xy), torch.flatten(target_box_xy))
+
+        # wh loss
+        predictor_wh = exists_box * predictor_wh
+        wh_loss = self.lambda_coord * self.mse(torch.flatten(torch.abs(predictor_wh)**(1/2)), torch.flatten(torch.abs(target_box_wh)**(1/2)))
+
+        # conf object loss
+        predictor_conf = exists_box * predictor_conf
+        obj_loss = self.mse(torch.flatten(predictor_conf), torch.flatten(target_box_confidence))
+
+        # confidence not object loss
+        no_exists_box = (1 - exists_box)
+        no_predictor_conf = no_exists_box * predictor_conf
+        noobj_loss = self.lambda_noobj * self.mse(torch.flatten(no_predictor_conf), torch.flatten(target_confidence)) 
+
+        # class loss
+        pred_boxes_classes = exists_box * pred_boxes_classes
+        class_loss = self.mse(torch.flatten(pred_boxes_classes), torch.flatten(target_box_classes))
+
+        # TODO: Keypoint loss
+
+        # Computing total loss
+        loss = xy_loss + wh_loss + obj_loss + noobj_loss + class_loss
         
         return loss
