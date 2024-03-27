d0701f7741b70eb543100f71771b852d3ef2d298
Pushkar Kadam <pushkarkadam17@outlook.com>
2024-03-27 18:15:57 +0000

    train_model function added

--- yolov1/train.py
+++ yolov1/train.py
@@ -11,6 +11,7 @@ from .utils import *
 from .loss import *
 from .convnet import *
 import yaml
+import pickle
 
 seed = 123 
 torch.manual_seed(seed)
@@ -150,130 +151,132 @@ class ModelTrain:
         torch.save(self.model.state_dict(), best_model_path)
         print(f"Saved Pytorch Model State to {best_model_path}")
 
-def train(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    model.train()
-    dt = time.time()
-    for batch, (X, y) in enumerate(dataloader):
-        X = X.to(device)
-        y = y.type(torch.LongTensor)
-        y = y.to(device)
+def train_model(dataloaders, 
+                model, 
+                criterion, 
+                optimizer,
+                scheduler,
+                dataset_sizes,
+                batch_size,
+                num_epochs=100, 
+                num_boxes=2, 
+                grid_size=7, 
+                num_landmarks=21, 
+                num_classes=2, 
+                verbose=True, 
+                save_model_path='../data/runs',
+                train_dir='train'
+               ):
+    """Training function"""
+
+    # Record start time
+    since = time.time()
+
+    if save_model_path:
+        timestamp = str(int(since))
+        train_dir = train_dir + '_' + timestamp
+        train_path = os.path.join(save_model_path, train_dir)
+    
+        # Last model path to save model after every epoch
+        last_model_path = os.path.join(train_path, "last.pt")
+    
+        # final model that is saved after the training is complete
+        best_model_path = os.path.join(train_path, "best.pt")
+
+    # Setting all the losses to dictionary
+    all_losses = {'train': dict(), 'valid': dict()}
+
+    best_acc = 0.0
+
+    for epoch in tqdm(range(num_epochs), unit='batch', total=num_epochs):
+        print(f"Epoch: {epoch + 1}")
+        print(f"{len('Epoch: ' + str(epoch+1)) * '-'}")
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train() # set model to training mode
+            else:
+                model.eval()
         
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+            running_loss = 0.0
+            running_corrects = 0
+            # last_loss = 0.0
+
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            
+            for i, data in enumerate(dataloaders[phase], start=0):
+                inputs = data['image'].to(device)
+
+                # Setting the target data to device
+                for k, v in data.items():
+                    if type(v) == torch.Tensor:
+                        data[k] = v.to(device)
+                
+                # Assigning the current batch size
+                current_batch_size = inputs.size(0)
+                
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
         
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+                    pred = yolo_head(outputs, 
+                                     num_boxes=num_boxes, 
+                                     num_landmarks=num_landmarks, 
+                                     num_classes=num_classes, 
+                                     grid_size=grid_size, 
+                                     batch_size=current_batch_size, 
+                                     image_name=data['image_name'])
+
+                    
+                    losses = criterion(outputs, data, batch_size=current_batch_size)
+                    # import pdb;pdb.set_trace()
+                    loss = losses['total_loss']
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+            
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
         
-        if batch % 100 == 0:
-            time_elapsed = time.time() - dt
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}] [Time Elapsed: {time_elapsed/3600:>4f} h]")
+            print(f'{phase}: ', end='')
+            for k, v in losses.items():
+                print(f"{k}: {v:.3f}, ", end="")
+                if k not in all_losses[phase]:
+                    all_losses[phase][k] = []
+                all_losses[phase][k].append(float(v))
+            print("\n")
 
-def test(dataloader, model, loss_fn, device):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X = X.to(device)
-            y = y.type(torch.LongTensor) 
-            y = y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
+            # deep copy the model
+            if save_model_path:
+                if not os.path.exists(train_path):
+                    os.makedirs(train_path)
+                    print(f"New directory {train_dir} created at {train_path}")
+                
+                torch.save(model.state_dict(), last_model_path)
+    
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
 
-# class Compose(object):
-#     def __init__(self, transforms):
-#         self.transforms = transforms
+    if save_model_path:
+        torch.save(model.state_dict(), best_model_path)
+        with open(os.path.join(train_path, 'all_losses.pickle'), 'wb') as handle:
+            pickle.dump(all_losses, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-#     def __call__(self, img, bboxes):
-#         for t in self.transforms:
-#             img, bboxes = t(img), bboxes
-
-#         return img, bboxes
-
-# transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor()])
-
-# def train_fn(train_loader, model, optimizer, loss_fn):
-#     loop = tqdm (train_loader, leave=True)
-#     mean_loss = []
-
-#     for batch_idx, (x, y) in enumerate(loop):
-#         x, y = x.to(DEVICE), y.to(DEVICE)
-#         out = model(x)
-#         loss = loss_fn(out, y)
-#         mean_loss.append(loss.item())
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-
-#         # update the progress bar 
-#         loop.set_postfix(loss = loss.item())
-
-#     print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
-
-# def main():
-#     model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
-#     optimizer = optim.Adam(
-#         model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-#     )
-#     loss_fn = YoloLoss()
-
-#     if LOAD_MODEL:
-#         load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
-
-#     train_dataset = VOCDataset(
-#         "../data/8examples.csv",
-#         transform=transform,
-#         img_dir=IMG_DIR,
-#         label_dir=LABEL_DIR
-#     )
-
-#     test_dataset = VOCDataset(
-#         "../data/test.csv",
-#         transform=transform,
-#         img_dir=IMG_DIR,
-#         label_dir=LABEL_DIR
-#     )
-
-#     train_loader = DataLoader(
-#         dataset=train_dataset,
-#         batch_size=BATCH_SIZE,
-#         num_workers=NUM_WORKERS,
-#         pin_memory=PIN_MEMORY,
-#         shuffle=True,
-#         drop_last=False,
-#     )
-
-#     test_loader = DataLoader(
-#         dataset=test_dataset,
-#         batch_size=BATCH_SIZE,
-#         num_workers=NUM_WORKERS,
-#         pin_memory=PIN_MEMORY,
-#         shuffle=True,
-#         drop_last=True,
-#     )
-
-#     for epoch in range(EPOCHS):
-#         pred_boxes, target_boxes = get_bboxes(
-#             train_loader, model, iou_threshold=0.5, threshold=0.4
-#         )
-
-#         mean_avg_prec = mean_average_precision(
-#             pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
-#         )
-
-#         print(f"Train mAP: {mean_avg_prec}")
-
-#         train_fn(train_loader, model, optimizer, loss_fn)
-
-# if __name__ == "__main__":
-#     main()
+    history = {'model': model,
+               'all_losses': all_losses
+              }
+    return history
+                
+    print('Finished Training')
